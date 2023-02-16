a925115b11e2f24fae4f04e6e65be072f50d2aa4
jcsuareze <suareze.jcarlos@outlook.com>
2023-02-15 18:19:17 -0600

    log4j reporting

--- opencart/src/test/java/com/opencart/testBase/BaseClass.java
+++ opencart/src/test/java/com/opencart/testBase/BaseClass.java
@@ -1,8 +1,10 @@
-package com.opencart.testCases;
+package com.opencart.testBase;
 
 import java.time.Duration;
 
 import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
@@ -13,21 +15,25 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class BaseClass {
 
 	public WebDriver driver;
+	public Logger logger; //for logging
 
 	@BeforeClass
-	void setup() {
+	public void setup() {
+		
+		logger = LogManager.getLogger(this.getClass()); //logging
 
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
+		//driver.manage().deleteAllCookies();
 		driver.get("http://localhost/opencart/upload/index.php");
 
 		driver.manage().window().maximize();
 	}
 
 	@AfterClass
-	void teardDown() {
+	public void teardDown() {
 
 		driver.quit();
 	}
--- opencart/src/test/java/com/opencart/testCases/TC_001_AccountRegistrationTest.java
+++ opencart/src/test/java/com/opencart/testCases/TC_001_AccountRegistrationTest.java
@@ -5,32 +5,41 @@ import org.testng.annotations.Test;
 
 import com.opencart.pageObjects.AccountRegistrationPage;
 import com.opencart.pageObjects.HomePage;
+import com.opencart.testBase.BaseClass;
 
 public class TC_001_AccountRegistrationTest extends BaseClass {
-	
+
 	@Test
 	void test_account_Registration() {
 
-	
-			HomePage hp = new HomePage(driver);
-			AccountRegistrationPage regPage = new AccountRegistrationPage(driver);
-			hp.clickMyAccount();
-			hp.clickRegistro();
+		logger.debug("<Registros");
+		logger.info("****Iniciando TC_001_AccountRegistrationTest*****");
 
-			regPage.setFirstName(randomString(8).toUpperCase());
-			regPage.setLastName(randomString(8).toUpperCase());
-			regPage.setEmail(randomString(5) + "@correo.com");// Generado de forma aleatoria
-			regPage.setUserName(randomString(10));
+		HomePage hp = new HomePage(driver);
+		AccountRegistrationPage regPage = new AccountRegistrationPage(driver);
+		hp.clickMyAccount();
 
-			String password = randomAlphaNumeric(6);
-			regPage.setPassword(password);
-			regPage.setConfirmPassword(password);
-			regPage.setPrivacyPolicy();
-			regPage.clickCountinue();
-			String confMsg = regPage.getConfirmationMsg();
+		logger.info("Click en Mi cuenta");
+		hp.clickRegistro();
+		logger.info("Click en Registro");
 
-			Assert.assertEquals(confMsg, "Congratulations! Your new account has been successfully created!");
-		
+		regPage.setFirstName(randomString(8).toUpperCase());
+		regPage.setLastName(randomString(8).toUpperCase());
+		regPage.setEmail(randomString(5) + "@correo.com");// Generado de forma aleatoria
+		regPage.setUserName(randomString(10));
+
+		String password = randomAlphaNumeric(6);
+		regPage.setPassword(password);
+		regPage.setConfirmPassword(password);
+		regPage.setPrivacyPolicy();
+		regPage.clickCountinue();
+		logger.info("Click en continuar");
+		String confMsg = regPage.getConfirmationMsg();
+
+		logger.info("Validando mensaje");
+		Assert.assertEquals(confMsg, "Congratulations! Your new account has been successfully created!");
+
+		logger.info("****Iniciando TC_001_AccountRegistrationTest*****");
 
 	}
 
